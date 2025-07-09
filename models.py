e32e3665748dff29d02e79103403af8fe5cb77a0
张艺萌 <zhangyimeng@Ivonnes-MacBook-Air.local>
2025-07-09 16:11:49 -0700

    Add user login feature and fix fridge visibility

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
+from flask_sqlalchemy import SQLAlchemy # type: ignore
+from flask_login import UserMixin # type: ignore
 
 db = SQLAlchemy()
 
